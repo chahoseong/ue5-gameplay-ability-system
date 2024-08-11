9ff877f6904de6afc4c294d96af5535f1cfe130e
Cha Hoseong <hoseong@chahoseong.com>
2024-08-11 18:01:23 +0900

    gameplay tags singleton

---
+++ Source/Aura/AuraGameplayTags.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AuraGameplayTags.h"
+#include "GameplayTagsManager.h"
+
+FAuraGameplayTags FAuraGameplayTags::GameplayTags;
+
+const FAuraGameplayTags& FAuraGameplayTags::Get()
+{
+	return GameplayTags;
+}
+
+void FAuraGameplayTags::InitializeNativeGameplayTags()
+{
+	UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.Armor"), FString("Reduces damage taken, improves Block Chance"));
+}
