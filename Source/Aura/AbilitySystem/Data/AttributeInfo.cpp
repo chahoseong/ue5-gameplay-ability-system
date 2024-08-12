35357a8e794466e66fc3f343d475c1240d0ce7d1
Cha Hoseong <hoseong@chahoseong.com>
2024-08-12 11:55:23 +0900

    attribute info data asset

---
+++ Source/Aura/AbilitySystem/Data/AttributeInfo.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AttributeInfo.h"
+
+FAuraAttributeInfo UAttributeInfo::FindAttributeInfoForTag(const FGameplayTag& AttributeTag, bool bLogNotFound) const
+{
+	for (const FAuraAttributeInfo& Info : AttributeInfomation)
+	{
+		// Info.AttributeTag.MatchesTagExact
+		if (Info.AttributeTag == AttributeTag)
+		{
+			return Info;
+		}
+	}
+
+	if (bLogNotFound)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Can't find Info for AttributeTag [%s] on AttributeInfo [%s]."), *AttributeTag.ToString(), *GetNameSafe(this));
+	}
+
+	return FAuraAttributeInfo();
+}
