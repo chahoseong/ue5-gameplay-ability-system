625218d37cbc2e158f949a4c057d6ad12ae32535
Cha Hoseong <hoseong@chahoseong.com>
2024-08-12 13:22:16 +0900

    aura ability system blueprint library

--- Source/Aura/UI/WidgetController/AttributeMenuWidgetController.cpp
+++ Source/Aura/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "UI/WidgetController/AttributeMenuWidgetController.h"
+#include "AttributeMenuWidgetController.h"
 
 void UAttributeMenuWidgetController::BindCallbacksToDependencies()
 {
