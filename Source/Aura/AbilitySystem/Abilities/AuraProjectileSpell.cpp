4c8e448f58ad9233bba0aaf4ed9f6dc63cc65019
Cha Hoseong <hoseong@chahoseong.com>
2024-08-26 13:07:32 +0900

    spawn firebolt from event

--- Source/Aura/AbilitySystem/Abilities/AuraProjectileSpell.cpp
+++ Source/Aura/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -9,8 +9,11 @@
 void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+}
 
-	const bool bIsServer = HasAuthority(&ActivationInfo);
+void UAuraProjectileSpell::SpawnProjectile()
+{
+	const bool bIsServer = GetAvatarActorFromActorInfo()->HasAuthority();
 	if (!bIsServer)
 	{
 		return;
@@ -24,6 +27,7 @@ void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Hand
 		FTransform SpawnTransform;
 		SpawnTransform.SetLocation(SocketLocation);
 		// TODO: Set the Projectile Rotation.
+
 
 		AAuraProjectile* Projectile = GetWorld()->SpawnActorDeferred<AAuraProjectile>(
 			ProjectileClass,
