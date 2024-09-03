a04a57a297f70cd095c9b24be2e1c9440a1dc17d
Cha Hoseong <hoseong@chahoseong.com>
2024-09-03 17:24:27 +0900

    mapping damage types to resistanes

--- Source/Aura/AuraGameplayTags.h
+++ Source/Aura/AuraGameplayTags.h
@@ -40,6 +40,11 @@ public:
 	FGameplayTag Attributes_Secondary_MaxHealth;
 	FGameplayTag Attributes_Secondary_MaxMana;
 
+	FGameplayTag Attributes_Resistance_Fire;
+	FGameplayTag Attributes_Resistance_Lightning;
+	FGameplayTag Attributes_Resistance_Arcane;
+	FGameplayTag Attributes_Resistance_Physical;
+
 	/*
 	 * Input
 	 */
@@ -57,8 +62,11 @@ public:
 
 	FGameplayTag Damage;
 	FGameplayTag Damage_Fire;
+	FGameplayTag Damage_Lightning;
+	FGameplayTag Damage_Arcane;
+	FGameplayTag Damage_Physical;
 
-	TArray<FGameplayTag> DamageTypes;
+	TMap<FGameplayTag, FGameplayTag> DamageTypesToResistances;
 
 	/*
 	 * Effects
