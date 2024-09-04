93bc29134ed8ddb68c6cd62351f4082be45752c9
Cha Hoseong <hoseong@chahoseong.com>
2024-09-04 12:24:19 +0900

    multiplayer test

--- Source/Aura/Character/AuraEnemy.cpp
+++ Source/Aura/Character/AuraEnemy.cpp
@@ -60,7 +60,10 @@ void AAuraEnemy::BeginPlay()
 
 	InitAbilityActorInfo();
 
-	UAuraAbilitySystemLibrary::GiveStartupAbilities(this, AbilitySystemComponent);
+	if (HasAuthority())
+	{
+		UAuraAbilitySystemLibrary::GiveStartupAbilities(this, AbilitySystemComponent);
+	}
 
 	if (UAuraUserWidget* AuraUserWidget = Cast<UAuraUserWidget>(HealthBar->GetUserWidgetObject()))
 	{
@@ -94,7 +97,10 @@ void AAuraEnemy::InitAbilityActorInfo()
 	AbilitySystemComponent->InitAbilityActorInfo(this, this);
 	Cast<UAuraAbilitySystemComponent>(AbilitySystemComponent)->AbilityActorInfoSet();
 
-	InitializeDefaultAttributes();
+	if (HasAuthority())
+	{
+		InitializeDefaultAttributes();
+	}
 }
 
 void AAuraEnemy::InitializeDefaultAttributes() const
